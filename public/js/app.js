11554597ed56c67d484f8225682e20f84aee3531
unknown <DeltaT71@gmail.com>
2023-04-08 10:11:49 +0300

    Removed empty lines

--- public/js/app.js
+++ public/js/app.js
@@ -3,9 +3,6 @@ const search = document.querySelector('input')
 const message_1 = document.querySelector('#message_1')
 const message_2 = document.querySelector('#message_2')
 
-
-
-
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()
   message_1.textContent = 'Loading...';
